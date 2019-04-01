233e6f2630c556220a5770a84a99eba3d884375e
abdul rehman <arehman@aoiths.org>
2019-04-01 22:25:36 +0000

    game

--- js/game.js
+++ js/game.js
@@ -6,24 +6,170 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You come across a dark forest.",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Enter The Forest",
+                    nextLevel: "forest",
                 },
 
                 {
-                    text: "Keep on moving",
+                    text: "It's Too Dark Move On",
                     nextLevel: "field",
                 },
             ]
         },
 
-        cave: {
+        forest: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "Keep exploring or go back?",
+            choices: [
+                {
+                    text: "Explore",
+                    nextLevel: "which_way",
+                },
+                
+                {
+                    text: "Go Back",
+                    nextLevel: "field",
+                },
+            ]
+        },
+        
+        which_way: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "Choose the direction you would like to explore!",
+            choices: [
+                {
+                    text: "Go Left",
+                    nextLevel: "weapon",
+                },
+                
+                {
+                    text: "Go Right",
+                    nextLevel: "field3",
+                },
+            ]
+        },
+        
+        weapon: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You come across a sword!",
+            choices: [
+                {
+                    text: "Take Sword",
+                    nextLevel: "bear",
+                },
+                
+                {
+                    text: "Go back",
+                    nextLevel: "which_way",
+                },
+            ]
+        },
+        
+        bear: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You come across a bear!",
+            choices: [
+                {
+                    text: "Fight",
+                    nextLevel: "choose",
+                },
+                
+                {
+                    text: "Run",
+                    nextLevel: "field2",
+                },
+            ]
+        },
+        
+        choose: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You're exausted after the fight!",
+            choices: [
+                {
+                    text: "Run For Your Life",
+                    nextLevel: "field2",
+                },
+                
+                {
+                    text: "Keep Exploring",
+                    nextLevel: "help",
+                },
+            ]
+        },
+        
+        help: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You stumble upon a stranger fighting for their life against a pack of wolf!",
+            choices: [
+                {
+                    text: "Save Yourself",
+                    nextLevel: "field4",
+                },
+                
+                {
+                    text: "Help The stranger",
+                    nextLevel: "ally",
+                },
+            ]
+        },
+        
+        ally: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You have teamed up with the stranger and saved them...you have made an ally, they point at the faint light up ahead....",
+            choices: [
+                {
+                    text: "Go towards the light",
+                    nextLevel: "light",
+                },
+            ]
+        },
+        
+        light: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "Walking towards the light....",
+            choices: [
+                {
+                    text: "You won",
+                    nextLevel: "win",
+                },
+            ]
+        },
+        
+        win: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "YOU HAVE EXPLORED THE FOREST AND SAVED A PERSON IN NEED OF HELP!!!",
+            choices: [
+                {
+                    text: "Start Over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+
+        field: {
+            message: "Some adventurer you are...",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        field2: {
+            message: "You live another day of a coward...",
             choices: [
                 {
                     text: "Start over",
@@ -32,8 +178,18 @@ var game = {
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        field3: {
+            message: "You tried to fight a bear without a weapon and died...",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        field4: {
+            message: "The wolfs outnumbered the stranger and then came after you, you died...",
             choices: [
                 {
                     text: "Start over",
